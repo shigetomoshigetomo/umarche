70ac82ebaa99196c172f7c3e4260a162da7a35fc
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-07-10 23:02:47 +0900

    [add]view file content [fix]web.php use

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\ComponentTestController;
+use App\Http\Controllers\ComponentTestController;
 
 /*
 |--------------------------------------------------------------------------
