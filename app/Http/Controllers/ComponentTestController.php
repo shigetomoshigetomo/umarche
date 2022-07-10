584a7151725d4237737383153075b0256cc940c8
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-07-10 22:45:29 +0900

    [add]componenttestcontroller showComponent1 method

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ComponentTestController extends Controller
 {
-    //
+    public function showComponent1(){
+        return view('tests.component-test1');
+    }
 }
