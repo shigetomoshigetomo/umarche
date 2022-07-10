8fadb60520ed9f45123c780f7950a374466414cf
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-07-10 22:48:08 +0900

    [add]componenttestcontroller showComponent2 method

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -9,4 +9,8 @@ class ComponentTestController extends Controller
     public function showComponent1(){
         return view('tests.component-test1');
     }
+
+    public function showComponent2(){
+        return view('tests.component-test2');
+    }
 }
